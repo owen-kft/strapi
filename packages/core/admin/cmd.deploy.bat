25480786d00a7582061c3b1a970b81f34b13339a
owen <owenwen@kfteausa.com>
2025-06-12 18:18:20 -0400

    cmd.deploy.bat

--- packages/core/admin/cmd.deploy.bat
+++ packages/core/admin/cmd.deploy.bat
@@ -1,4 +1,6 @@
-@REM xcopy D:\projects_tryOut\strapi\packages\core\admin\dist D:\projects\kft-cms\node_modules\@strapi\admin\dist /E /I /Y
+rmdir D:\projects\kft-cms\node_modules\@strapi\admin\dist /s /q
+xcopy D:\projects_tryOut\strapi\packages\core\admin\dist D:\projects\kft-cms\node_modules\@strapi\admin\dist /E /I /Y
 
 rmdir D:\projects\kft-cms\kft-patch\strapi-admin-dist-4-25-22 /s /q
 xcopy D:\projects_tryOut\strapi\packages\core\admin\dist D:\projects\kft-cms\kft-patch\strapi-admin-dist-4-25-22 /E /I /Y
+
